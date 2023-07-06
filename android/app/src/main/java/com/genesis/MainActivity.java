61d813183f853e104abf92f0d3aab11de51a1fdf
Azhan Hussain <azhan@salsoft.net>
2023-07-06 16:01:49 +0500

    Splash and appicon

--- android/app/src/main/java/com/genesis/MainActivity.java
+++ android/app/src/main/java/com/genesis/MainActivity.java
@@ -4,11 +4,29 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import static com.blitzapp.animatedsplash.animation.AnimatedText.FIT_CENTER;
+import static com.blitzapp.animatedsplash.animation.Constants.FADE;
+import static com.blitzapp.animatedsplash.animation.Constants.SCALE;
+import static com.blitzapp.animatedsplash.animation.Constants.SPLASH_SLIDE_RIGHT;
+import static com.blitzapp.animatedsplash.animation.Splash.hide;
+import static com.blitzapp.animatedsplash.animation.Splash.screenHeight;
+import static com.blitzapp.animatedsplash.animation.Splash.screenWidth;
+import static com.blitzapp.animatedsplash.animation.Splash.setBackgroundImage;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.blitzapp.animatedsplash.animation.AnimatedObject;
+import com.blitzapp.animatedsplash.animation.GroupAnimation;
+import com.blitzapp.animatedsplash.animation.ObjectAnimation;
+import com.blitzapp.animatedsplash.animation.SingleAnimation;
+import com.blitzapp.animatedsplash.animation.Splash;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -17,10 +35,22 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
    * (aka React 18) with two boolean flags.
    */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+    try {
+      initiateSplash();
+    } catch (Exception e) {
+      Log.e("ANIMATED_SPLASH", e.getMessage());
+    }
+  }
+
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new DefaultReactActivityDelegate(
@@ -28,8 +58,26 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e.
+        // React 18).
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        );
+    );
+  }
+
+  public void initiateSplash() throws Exception {
+
+    // Create Splash
+    Splash splash = new Splash(this);
+    // Set Background Color To View
+    setBackgroundImage(R.drawable.splash);
+    // Set Hiding Animation for Splash
+    splash.setSplashHideAnimation(SPLASH_SLIDE_RIGHT);
+    // Set Delay for Splash to hide
+    splash.setSplashHideDelay(1500);
+
+    // Creating Image and adding its properties and animation
+
+    splash.ShowSplash();
+
   }
 }
